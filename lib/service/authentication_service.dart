d912ee2ce26dca43da6cf469ea3bd243e6c85014
oyen-bright <oyenbrihight@gmail.com>
2024-07-06 21:06:33 +0100

    Authentication Service created

---
+++ lib/service/authentication_service.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthenticationService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
