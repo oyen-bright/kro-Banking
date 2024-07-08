bb1732bda68451a865ad851c4c772775e774d65d
oyen-bright <oyenbrihight@gmail.com>
2024-07-08 21:33:59 +0100

    side navigation bar refactored

--- lib/views/layout/layout_view.dart
+++ lib/views/layout/layout_view.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kro_banking/constants/app_constants.dart';
 import 'package:kro_banking/extentions/on_context.dart';
 import 'package:kro_banking/router/route.dart';
 import 'package:kro_banking/router/router.dart';
+import 'package:kro_banking/utils/get_greetings.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 final menuItems = [
-  {'title': 'Dashboard', 'icon': Icons.analytics, 'route': AppRoutes.home},
-  {'title': 'Account', 'icon': Icons.analytics, 'route': AppRoutes.accounts},
+  {'title': 'Dashboard', 'icon': Icons.dashboard, 'route': AppRoutes.home},
+  {'title': 'Account', 'icon': Icons.wallet, 'route': AppRoutes.accounts},
   {
     'title': 'Transfer',
-    'icon': Icons.analytics,
+    'icon': Icons.savings,
     'route': AppRoutes.transferFunds
   },
   {
     'title': 'Transactions',
-    'icon': Icons.analytics,
+    'icon': Icons.input,
     'route': AppRoutes.transactionHistory
   },
-  {'title': 'Bills', 'icon': Icons.analytics, 'route': AppRoutes.billPayments},
+  {'title': 'Bills', 'icon': Icons.payment, 'route': AppRoutes.billPayments},
   {
     'title': 'Investment',
     'icon': Icons.analytics,
     'route': AppRoutes.investments
   },
+  {'title': 'Support', 'icon': Icons.support_agent, 'route': null},
+  {'title': 'Settings', 'icon': Icons.settings, 'route': null},
+  {'title': 'Logout', 'icon': Icons.logout, 'route': null},
 ];
 
 bool isCurrentRoute(String baseRoute, BuildContext context) {
@@ -34,8 +39,6 @@ bool isCurrentRoute(String baseRoute, BuildContext context) {
   final List<String> segments =
       currentRoute.path.split('/').where((s) => s.isNotEmpty).toList();
 
-  print(segments);
-
   if (segments.isNotEmpty && "/${segments[0]}" == baseRoute) {
     return true;
   } else if (segments.isEmpty && baseRoute == "/") {
@@ -43,12 +46,14 @@ bool isCurrentRoute(String baseRoute, BuildContext context) {
   } else if (segments.isNotEmpty &&
       "/${segments[0]}" == AppRoutes.login &&
       baseRoute == "/") {
-    //TODO:fix login bug
+    //TODO:login route bug
     return true;
   } else {
     return false;
   }
 }
+
+final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
 class Layout extends StatelessWidget {
   final Widget child;
@@ -57,104 +62,181 @@ class Layout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // drawer: Drawer(
-      //   child: ListView(
-      //     padding: EdgeInsets.zero,
-      //     children: <Widget>[
-      //       const DrawerHeader(
-      //         decoration: BoxDecoration(
-      //           color: Colors.blue,
-      //         ),
-      //         child: Text(
-      //           'Drawer Header',
-      //           style: TextStyle(
-      //             color: Colors.white,
-      //             fontSize: 24,
-      //           ),
-      //         ),
-      //       ),
-      //       ListTile(
-      //         leading: const Icon(Icons.home),
-      //         title: const Text('Home'),
-      //         onTap: () {
-      //           context.go('/');
-      //         },
-      //       ),
-      //       ListTile(
-      //         leading: const Icon(Icons.dashboard),
-      //         title: const Text('Dashboard'),
-      //         onTap: () {
-      //           context.go('/dashboard');
-      //         },
-      //       ),
-      //       ListTile(
-      //         leading: const Icon(Icons.person),
-      //         title: const Text('Profile'),
-      //         onTap: () {
-      //           context.go('/profile');
-      //         },
-      //       ),
-      //       ListTile(
-      //         leading: const Icon(Icons.person),
-      //         title: const Text('sex'),
-      //         onTap: () {
-      //           context.go('/transfer');
-      //         },
-      //       ),
-      //     ],
-      //   ),
-      // ),
-      body: Row(
-        children: [
-          Drawer(
-              width: context.pWidth(300),
-              child: Container(
-                width: context.pWidth(300),
-                color: Colors.white,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    SizedBox(
-                      height: 3.sh,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: context.pWidth(20),
-                      ),
-                      child: Text(
-                        KContents.kAppName.toUpperCase(),
-                        style: context.textTheme.headlineMedium?.copyWith(
-                            color: context.colorScheme.primary,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 3.sh,
-                    ),
-                    Expanded(
-                        child: ListView.builder(
-                            itemCount: menuItems.length,
-                            itemBuilder: (context, index) {
-                              final item = menuItems[index];
+    return ResponsiveBuilder(builder: (context, device) {
+      return Scaffold(
+        key: _scaffoldKey,
+        endDrawer: const Drawer(),
+        // appBar: AppBar(),
+        drawer: _drawer(context, device),
+        body: Row(
+          children: [
+            Builder(builder: (context) {
+              if (device.isDesktop || device.isTablet) {
+                double drawerWidth = device.isTablet ? 80 : context.pWidth(285);
+                return _sideNav(drawerWidth, context, device);
+              } else {
+                return const SizedBox.shrink();
+              }
+            }),
+            Expanded(
+              child: Column(
+                children: [
+                  Container(
+                    height: kToolbarHeight,
+                    color: Colors.red,
+                    child: ResponsiveBuilder(builder: (context, device) {
+                      return Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          if (device.isMobile)
+                            IconButton(
+                                onPressed: () =>
+                                    _scaffoldKey.currentState?.openDrawer(),
+                                icon: const Icon(Icons.menu)),
+                          SizedBox(
+                            width: 1.sw,
+                          ),
+                          Text(
+                            "${getGreeting()}, User",
+                            style: context.textTheme.titleMedium?.copyWith(
+                                fontSize:
+                                    context.textTheme.titleMedium!.fontSize! +
+                                        2.0),
+                          ),
+                          const Spacer(),
+                          IconButton(
+                              onPressed: () =>
+                                  _scaffoldKey.currentState?.openEndDrawer(),
+                              icon: const Icon(FontAwesomeIcons.bell)),
+                          CircleAvatar(
+                            radius: 20,
+                            child: Image.network(
+                                "https://avatars.githubusercontent.com/u/35768100?v=4"),
+                          )
+                        ],
+                      );
+                    }),
+                  ),
+                  Expanded(child: child),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
 
-                              return ListTile(
-                                onTap: () => AppRouter.router
-                                    .go(item['route'] as String),
-                                selectedColor: Colors.red,
-                                selected: isCurrentRoute(
-                                    item['route'] as String, context),
-                                title: Text(item['title'] as String),
-                                leading: Icon(item['icon'] as IconData),
-                              );
-                            }))
-                  ],
+  AnimatedContainer _sideNav(
+      double drawerWidth, BuildContext context, SizingInformation device) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      width: drawerWidth,
+      decoration: BoxDecoration(
+          border: Border(
+              right: BorderSide(
+                  color: context.colorScheme.primary.withOpacity(0.2)))),
+      child: _drawer(context, device),
+    );
+  }
+
+  Drawer _drawer(BuildContext context, SizingInformation device) {
+    return Drawer(
+      shape: const RoundedRectangleBorder(),
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 2.1.sh),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: context.pWidth(30)),
+              child: Text(
+                KContents.kAppName.toUpperCase(),
+                maxLines: 1,
+                style: context.textTheme.headlineMedium?.copyWith(
+                  color: context.colorScheme.primary,
+                  fontWeight: FontWeight.bold,
                 ),
-              )),
-          Expanded(
-            child: child,
-          ),
-        ],
+              ),
+            ),
+            SizedBox(height: 3.sh),
+            Expanded(
+              child: ListView.separated(
+                separatorBuilder: (context, index) {
+                  if (index == 5) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: context.pWidth(20),
+                          vertical: context.pHeight(10)),
+                      child: Divider(
+                        color: context.colorScheme.primary.withOpacity(0.2),
+                      ),
+                    );
+                  }
+                  return const SizedBox.shrink();
+                },
+                itemCount: menuItems.length,
+                itemBuilder: (context, index) {
+                  final item = menuItems[index];
+                  final title = item['title'] as String;
+                  final route = item['route'] as String?;
+                  final isSelected = route != null
+                      ? isCurrentRoute((item['route'] as String?)!, context)
+                      : false;
+
+                  return Material(
+                    color: Colors.transparent,
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: context.pWidth(20)),
+                      child: device.isTablet
+                          ? Tooltip(
+                              message: title,
+                              child: _buildItem(context, isSelected, route,
+                                  item, device, title),
+                            )
+                          : _buildItem(
+                              context, isSelected, route, item, device, title),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  ListTile _buildItem(BuildContext context, bool isSelected, String? route,
+      Map<String, Object?> item, SizingInformation device, String title) {
+    return ListTile(
+      selectedColor: context.colorScheme.primary,
+      selectedTileColor: isSelected
+          ? context.colorScheme.primary.withOpacity(0.2)
+          : Colors.grey,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+        side: BorderSide(
+          width: 1,
+          color: isSelected
+              ? context.colorScheme.primary.withOpacity(0.5)
+              : Colors.transparent,
+        ),
+      ),
+      onTap: () =>
+          route != null ? AppRouter.router.go(item['route'] as String) : null,
+      selected: isSelected,
+      title: device.isTablet
+          ? const SizedBox.shrink()
+          : Text(
+              title,
+              maxLines: 1,
+            ),
+      leading: Icon(
+        item['icon'] as IconData,
+        color: isSelected ? context.colorScheme.primary : Colors.grey,
       ),
     );
   }
